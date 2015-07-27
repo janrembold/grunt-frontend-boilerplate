a45ec7c71e316e1430a3ddeabf20c152fc17b112
Jan Rembold <janrembold@googlemail.com>
2015-07-27 14:35:49 +0200

    added initial offcanvas styles added fastclick js

--- src/resources/js/main.js
+++ src/resources/js/main.js
@@ -4,9 +4,23 @@
     $(document).ready(function(){
         console.log('document ready');
 
-        if(Modernizr.touch) {
-            console.log('is touch');
-        }
+        // enable fastclick
+        FastClick.attach(document.body);
+
+        // offcanvas dummy click events
+        $('.offcanvas__show-left-nav').on('click', function(e){
+            e.preventDefault();
+            $(this).closest('.offcanvas').toggleClass('offcanvas--show-left');
+        });
+
+        $('.offcanvas__show-right-nav').on('click', function(e){
+            e.preventDefault();
+            $(this).closest('.offcanvas').toggleClass('offcanvas--show-right');
+        });
+
+        $('.offcanvas__exit').on('click', function(){
+            $(this).closest('.offcanvas').removeClass('offcanvas--show-left offcanvas--show-right');
+        });
     });
 
 })(jQuery);
